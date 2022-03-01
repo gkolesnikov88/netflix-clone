ee20d058b1f9da68ec82fab6858c8687af7e32d5
George Kolesnikov <gkolesnikov88@gmail.com>
2022-03-01 06:36:46 +0300

    Initialize

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDB4FWsmJeNB-vLL4XnEOXu-EAfjTSIfX0",
+    authDomain: "netflix-clone-c586f.firebaseapp.com",
+    projectId: "netflix-clone-c586f",
+    storageBucket: "netflix-clone-c586f.appspot.com",
+    messagingSenderId: "35254746768",
+    appId: "1:35254746768:web:8dc1521d57bfa3430f6215"
+};
