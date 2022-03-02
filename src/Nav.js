5b20b1b3d16c013007c1aae8a6dcff439662df58
George Kolesnikov <gkolesnikov88@gmail.com>
2022-03-02 06:35:46 +0300

    Add home screen and navigation component

---
+++ src/Nav.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from 'react';
+import './Nav.css';
+
+function Nav(props) {
+    const [show, handleShow] = useState(false);
+
+    const transitionNavBar = () => {
+        if (window.scrollY > 100) {
+            handleShow(true);
+        } else {
+            handleShow(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', transitionNavBar);
+        return () => window.removeEventListener('scroll', transitionNavBar);
+    }, []);
+
+    return (
+        <div className={`nav ${show && 'nav__black'}`}>
+            <div className='nav__content'>
+                <img
+                    className='nav__logo'
+                    src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+                    alt=""
+                />
+                <img
+                    className='nav__avatar'
+                    src="https://p0.zoon.ru/preview/DknY--qgAxYmiVdTzuykRw/130x170x90/1/c/7/5c6e29dac392295e0a6a766c_5c6e2e005a846.jpg"
+                    alt=""
+                />
+            </div>
+        </div>
+    );
+}
+
+export default Nav;
