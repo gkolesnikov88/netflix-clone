b0708d9a03c7506ced412e63b56f25cf0f0d7c41
George Kolesnikov <gkolesnikov88@gmail.com>
2022-03-03 06:39:29 +0300

    Add banner component

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './HomeScreen.css';
 import Nav from "./Nav";
+import Banner from "./Banner";
 
 function HomeScreen(props) {
     return (
         <div className='homeScreen'>
             <Nav />
 
-            {/* Banner */}
+            <Banner/>
 
             {/* Row */}
         </div>
