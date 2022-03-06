cb87072d4227b2a6dae8ea458874cc061e5e9a45
George Kolesnikov <gkolesnikov88@gmail.com>
2022-03-06 18:50:21 +0300

    Add login screen component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
-  return (
-    <div className="app">
-        <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<HomeScreen />}/>
-            </Routes>
-        </BrowserRouter>
-    </div>
-  );
+    const user = null;
+
+    return (
+        <div className="app">
+            <BrowserRouter>
+                {
+                    !user ? (
+                        <LoginScreen/>
+                    ) :
+                        <Routes>
+                            <Route path="/" element={<HomeScreen />}/>
+                        </Routes>
+                }
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
