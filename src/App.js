3b4f8808b97208b656af22c6b78a83c27a4aef3c
George Kolesnikov <gkolesnikov88@gmail.com>
2022-04-09 08:22:32 +0300

    Add sign in and sign up functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
+import { auth, onAuthStateChanged } from "./firebase";
 
 function App() {
     const user = null;
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, userAuth => {
+            if (userAuth) {
+                // logged in
+                console.log(userAuth);
+            } else {
+                // logged out
+            }
+        });
+
+         return unsubscribe;
+    }, [])
 
     return (
         <div className="app">
---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged
+} from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDB4FWsmJeNB-vLL4XnEOXu-EAfjTSIfX0",
+    authDomain: "netflix-clone-c586f.firebaseapp.com",
+    projectId: "netflix-clone-c586f",
+    storageBucket: "netflix-clone-c586f.appspot.com",
+    messagingSenderId: "35254746768",
+    appId: "1:35254746768:web:8dc1521d57bfa3430f6215"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth();
+
+export {
+    auth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+};
+export default db;
