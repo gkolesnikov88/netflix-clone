43de485e093946a50afbd2e559b3b8b314dbe2e6
George Kolesnikov <gkolesnikov88@gmail.com>
2022-04-10 19:40:11 +0300

    Add user slice (redux)

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
 import { auth, onAuthStateChanged } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-    const user = null;
+    const user = useSelector(selectUser);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, userAuth => {
             if (userAuth) {
                 // logged in
-                console.log(userAuth);
+                dispatch(login({
+                    uid: userAuth.uid,
+                    email: userAuth.email,
+                }));
             } else {
                 // logged out
+                dispatch(logout);
             }
         });
 
